8e658908ec8376ebb40110e313b561da663d93c1
Kevin Cox <kevincox2086@gmail.com>
2025-06-19 14:54:36 -0400

    complete Learn 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 4, 5));
+        shapes.Add(new Circle("Green", 3));
+        shapes.Add(new Rectangle("Yellow", 6, 2.5));
+
+        Console.WriteLine("Calculating Areas of Shapes:");
+        Console.WriteLine("-----------------------------");
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            string type = shape.GetType().Name;
+
+            Console.WriteLine($"Shape: {type}, Color: {color}, Area: {area:F2}");
+        }
     }
 }
